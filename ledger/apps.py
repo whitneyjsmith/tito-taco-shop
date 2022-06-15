2d6b57ecab6d175402a63cb4c7814ddd62458cc3
Kenny <kennethpavia@gmail.com>
2022-06-15 12:42:15 -0600

    fixed typo 'leger' and renamed everything to 'ledger'

--- ledger/apps.py
+++ ledger/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LegerConfig(AppConfig):
+class LedgerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'leger'
+    name = 'ledger'
