f3dbb4a400a346dd75c3b9fe7e7fa618353aebf6
Whitney Smith <16254930+whitneyjsmith@users.noreply.github.com>
2022-08-15 16:58:55 -0600

    [TITO-17] added Attribute and AttributeBase models. Modified Product model.

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Attribute, AttributeBase
 
 admin.site.register(Product)
+admin.site.register(Attribute)
+admin.site.register(AttributeBase)
--- products/models.py
+++ products/models.py
@@ -2,8 +2,20 @@ from django.db import models
 from django.core.files.storage import default_storage
 
 
+class AttributeBase(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Attribute(models.Model):
+    value = models.CharField(max_length=100)
+    internal_value = models.CharField(max_length=100)
+    attribute_base = models.ForeignKey(AttributeBase, on_delete=models.CASCADE)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images')
     price = models.IntegerField()
     description = models.CharField(max_length=1000)
+    attributes = models.ManyToManyField(Attribute)
+    attribute_bases = models.ManyToManyField(AttributeBase)
