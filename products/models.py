ce1ceb31f5d76b6489eb9b7710e54d7a07e30438
Whitney Smith <16254930+whitneyjsmith@users.noreply.github.com>
2022-10-01 14:12:06 -0600

    Added Category and Quote models for randomly generating quotes on checkout page

--- products/models.py
+++ products/models.py
@@ -30,7 +30,7 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    category = models.CharField(max_length=50, null=True, blank=True)
+    category = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.category
