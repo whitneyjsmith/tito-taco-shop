1900ffbcb6c8f9fdb828279c98b0dd9072d2ca87
Whitney Smith <16254930+whitneyjsmith@users.noreply.github.com>
2022-10-17 10:20:36 -0600

    [TITO-27] added basic functioning checkout page

--- products/views.py
+++ products/views.py
@@ -13,3 +13,21 @@ def get_image(request, product_id, filename):
     product = Product.objects.get(id=product_id)
     image_data = product.image.open()
     return HttpResponse(image_data, content_type="image/gif")
+
+
+def checkout(request):
+    #TODO: need to get user id
+    products = [{
+        'id': p.id,
+        'name': p.name,
+        'image': p.image,
+        'price': p.price,
+        'description': p.description
+    } for p in Product.objects.all()]
+    step = 3  # max number of products in a row
+    chunks = []
+    for i in range(0, len(products), step):
+        x = i
+        chunks.append(products[x:x + step])
+
+    return render(request, 'products/checkout.html', context={'products': chunks})
