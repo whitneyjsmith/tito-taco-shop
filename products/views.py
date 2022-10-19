e795cedaa6308e9169a827971f5b86eb3ccfac92
Whitney Smith <16254930+whitneyjsmith@users.noreply.github.com>
2022-10-19 11:49:53 -0600

    Updated checkout page and removed cart so 1 product gets ordered at a time

--- products/views.py
+++ products/views.py
@@ -15,19 +15,6 @@ def get_image(request, product_id, filename):
     return HttpResponse(image_data, content_type="image/gif")
 
 
-def checkout(request):
-    #TODO: need to get user id
-    products = [{
-        'id': p.id,
-        'name': p.name,
-        'image': p.image,
-        'price': p.price,
-        'description': p.description
-    } for p in Product.objects.all()]
-    step = 3  # max number of products in a row
-    chunks = []
-    for i in range(0, len(products), step):
-        x = i
-        chunks.append(products[x:x + step])
-
-    return render(request, 'products/checkout.html', context={'products': chunks})
+def checkout(request, product_id):
+    product = Product.objects.filter(id=product_id).first()
+    return render(request, 'products/checkout.html', context={'product': product})
