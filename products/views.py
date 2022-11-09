7cb86a60964a9131333df6501bae8cdc1bc6c04e
Whitney Smith <16254930+whitneyjsmith@users.noreply.github.com>
2022-11-09 13:31:53 -0700

    [TITO-34] added ORDER_CHANNEL env var

--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from products.models import Product
-
+from ledger.tasks import redeem_tacos
+from integration.clients.slack import Client as Slack
+from integration.models import Team
+from django.conf import settings
 
 def product(request, product_id):
     product = Product.objects.filter(id=product_id).first()
@@ -17,4 +20,10 @@ def get_image(request, product_id, filename):
 
 def checkout(request, product_id):
     product = Product.objects.filter(id=product_id).first()
+    redeem_tacos(request)
+    team_obj = Team.objects.first()
+    slack_client = Slack(team_obj.team_id, team_obj.name, team_obj.bot_user_id)
+    user = request.user
+    slack_client.order_information(user.unique_id, settings.ORDER_ID, product.name)
+    slack_client.receipt(user.unique_id, product.name, product.price, user.taco_bank_set.first().total_tacos)
     return render(request, 'products/checkout.html', context={'product': product})
