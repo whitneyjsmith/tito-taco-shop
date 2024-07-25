de904331f891c982b18c585086fe05f24360d7c4
monutech <69862128+monutech@users.noreply.github.com>
2024-07-25 15:14:22 -0600

    remove extra productviewset references that confuses the UI

--- products/urls.py
+++ products/urls.py
@@ -7,8 +7,6 @@ router.register(r'',  views.ProductViewset)
 
 urlpatterns = router.urls
 urlpatterns = [
-    path('', views.ProductViewset.as_view({'get': 'list'})),
-    path('<int:pk>/', views.ProductViewset.as_view({'get': 'retrieve'})),
     path('<int:product_id>/', views.product, name='product-page'),
     path('<int:product_id>/product_images/<str:filename>', views.get_image, name='product-img'),
     path('checkout/<int:product_id>/', views.checkout, name='checkout-page'),
